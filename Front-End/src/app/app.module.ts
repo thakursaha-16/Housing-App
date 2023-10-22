e4a9ed6b685ff48cf25fc8d4f777003cb0d49937
Vineet <vineetbisen17@gmail.com>
2023-10-22 23:28:54 +0530

    Add property list and card

--- Front-End/src/app/app.module.ts
+++ Front-End/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './Property/property-list/property-list.component';
+import { PropertyCardComponent } from './Property/property-card/property-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PropertyListComponent,
+    PropertyCardComponent
   ],
   imports: [
     BrowserModule,
